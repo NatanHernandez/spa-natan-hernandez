b1a40072ce32aa0aa25861b7a83bc76c9dbb45cd
NatanHernandez <ecocharlydeltablue@gmail.com>
2025-09-21 15:39:52 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./styles.css"; // importa aquí
+import "./styles.css"; 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
